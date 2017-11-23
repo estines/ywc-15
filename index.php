70fdc337d6d56288c8b8cab7824151c4a45f531f
estines <estines.nop@gmail.com>
2017-11-23 13:17:48 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    $Title = "Young Webmaster Camp";
+?>
+
+<html>
+    <head>
+        
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="style.css" type="text/css">
+        
+        <title> <?php  echo $Title; ?></title>
+    </head>
+    <body>
+        <div class="logo"><img src="img/logo.png" style="width:160px; height:120px;"></div>
+        <div class="wrapper-content container">
+            <div id="topic">
+                <h1>ประกาศรายชื่อผู้มีสิทธิ์เข้าสัมภาษณ์</h1>
+                <p>การสัมภาษณ์จะจัดขึ้นในวันที่ 26 พฤศจิกายน 2560 ณ อาคาร ซี.พี.ทาวเวอร์ 1 (สีลม)</p>
+                <p>ซึ่งจะแบ่งเป็น 2 รอบ คือ <span>รอบช่วงเช้าตั้งแต่เวลา 9.00 น. ถึง 12.00 น.</span> และ <span>รอบช่วงบ่ายตั้งแต่เวลา 13.00น. ถึง 18.00 น.</span></p>
+            </div>
+            <div id="detail">
+                <h2>สิ่งที่ต้องเตรียมมาในวันสัมภาษณ์</h2>
+                <p><span>1. บัตรประชาชน</span>สำหรับการแลกบัตรเข้าอาคาร ซี.พี.ทาวเวอร์ 1 (สีลม) และ บัตรนักศึกษาสำหรับการลงทะเบียนสัมภาษณ์ กรุณาแต่งกายด้วยชุดนักศึกษา</p>
+            </div>
+            <div class="row">
+                <div class="col-xs-3" id="ct-board"></div>
+                <div class="col-xs-3" id="ds-board"></div>
+                <div class="col-xs-3" id="mk-board"></div>
+                <div class="col-xs-3" id="pg-board"></div>
+            </div>
+        </div>
+        
+        <!-- Bootstrap Javascript & JQuery -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    </body>
+</html>
