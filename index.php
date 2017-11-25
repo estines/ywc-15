f5bc1ba618f978e04dff9a56423f2479027a1c79
estines <estines.nop@gmail.com>
2017-11-25 11:32:32 +0700

    commit

--- index.php
+++ index.php
@@ -71,12 +71,14 @@
             </div>
         </div>
         
-        <!-- Bootstrap Javascript & JQuery -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <!-- jQuery Library --->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <!-- Bootstrap Javascript -->
+        <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         <!-- Jquery 3.2.1 library -->
-        <script src="js/jquery-3.2.1.min.js"></script>
+        <!--<script src="js/jquery-3.2.1.min.js"></script> -->
         <script src="js/script.js" type="text/javascript"></script>
     </body>
 </html>
